753a0ab44d524db8b5615d27f42a1bcfaa1a9aa5
evgeniylee <evgeniylee94@gmail.com>
2026-06-17 12:38:09 +0500

    Fix asyncio for Python 3.14

--- main.py
+++ main.py
@@ -1206,19 +1206,31 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         return
 
-
+import asyncio
 # =========================
 # запуск
 # =========================
 def main():
+
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            handle_message
+        )
+    )
 
     print("Бот запущен...")
+
     app.run_polling()
 
 
 if __name__ == "__main__":
+
+    loop = asyncio.new_event_loop()
+
+    asyncio.set_event_loop(loop)
+
     main()
